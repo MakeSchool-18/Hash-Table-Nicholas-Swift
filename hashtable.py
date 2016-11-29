8dac4ce3232385da97b1e4956142bd0c9e6abd9d
Nicholas Swift <nicholas.w.swift@gmail.com>
2016-11-28 18:35:41 -0800

    Completed hashtable, all tests pass

--- hashtable.py
+++ hashtable.py
@@ -7,47 +7,110 @@ class HashTable(object):
 
     def __init__(self, init_size=8):
         """Initialize this hash table with the given initial size"""
+
         self.buckets = [LinkedList() for i in range(init_size)]
 
     def __repr__(self):
         """Return a string representation of this hash table"""
+
         return 'HashTable({})'.format(self.length())
 
     def _bucket_index(self, key):
         """Return the bucket index where the given key would be stored"""
+
         return hash(key) % len(self.buckets)
 
     def length(self):
         """Return the length of this hash table by traversing its buckets"""
-        # TODO: Count number of key-value entries in each of the buckets
-        pass
 
-    def contains(self):
+        num = 0
+        for i in self.buckets:
+            num += i.length()
+        return num
+
+    def contains(self, key):
         """Return True if this hash table contains the given key, or False"""
-        # TODO: Check if the given key exists in a bucket
-        pass
+
+        for bucket in self.buckets:
+            for node in bucket:
+                if key == node.data[0]:
+                    return True
+        return False
 
     def get(self, key):
         """Return the value associated with the given key, or raise KeyError"""
-        # TODO: Check if the given key exists and return its associated value
-        pass
+
+        # Find bucket
+        bucketIndex = self._bucket_index(key)
+        bucket = self.buckets[bucketIndex]
+
+        # Check if key is already in bucket
+        for node in bucket:
+            if key == node.data[0]:
+                return node.data[1]
+
+        raise KeyError('Key is not in HashTable')
 
     def set(self, key, value):
         """Insert or update the given key with its associated value"""
-        # TODO: Insert or update the given key-value entry into a bucket
-        pass
+
+        # Find bucket
+        bucketIndex = self._bucket_index(key)
+        bucket = self.buckets[bucketIndex]
+
+        # Check if key is already in bucket
+        for i, node in enumerate(bucket):
+            if key == node.data[0]:
+                new_tuple = (node.data[0], value)
+                node.data = new_tuple
+                return
+
+        # Add new key, value to bucket
+        bucket.append((key, value))
+        return
 
     def delete(self, key):
         """Delete the given key from this hash table, or raise KeyError"""
-        # TODO: Find the given key and delete its entry if found
-        pass
+
+        # Find bucket
+        bucketIndex = self._bucket_index(key)
+        bucket = self.buckets[bucketIndex]
+
+        # Check if key is in bucket
+        for node in bucket:
+            if key == node.data[0]:
+                bucket.delete(node.data)
+                return
+
+        # Raise error if not found
+        raise KeyError('Key is not in HashTable')
 
     def keys(self):
         """Return a list of all keys in this hash table"""
-        # TODO: Collect all keys in each of the buckets
-        pass
+
+        keys = []
+        for ll in self.buckets:
+            for node in ll:
+                keys.append(node.data[0])
+        return keys
 
     def values(self):
         """Return a list of all values in this hash table"""
-        # TODO: Collect all values in each of the buckets
-        pass
+
+        values = []
+        for ll in self.buckets:
+            for node in ll:
+                values.append(node.data[1])
+        return values
+
+
+if __name__ == '__main__':
+    ht = HashTable()
+    ht.set('one', 1)
+    ht.set('two', 2)
+    ht.set('three', 3)
+    ht.set('two', 5)
+    print(ht.keys())
+    print(ht.values())
+
+
---
+++ linkedlist.py
@@ -0,0 +1,184 @@
+#!python
+
+from __future__ import print_function
+
+
+class Node(object):
+
+    def __init__(self, data):
+        """Initialize this node with the given data"""
+        self.data = data
+        self.next = None
+        self.previous = None
+
+    def __repr__(self):
+        """Return a string representation of this node"""
+        return 'Node({})'.format(repr(self.data))
+
+
+class LinkedList(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this linked list; append the given items, if any"""
+        self.head = None
+        self.tail = None
+        if iterable:
+            for item in iterable:
+                self.append(item)
+
+    def __repr__(self):
+        """Return a string representation of this linked list"""
+        return 'LinkedList({})'.format(self.as_list())
+
+    def as_list(self):
+        """Return a list of all items in this linked list"""
+        result = []
+        current = self.head
+        while current is not None:
+            result.append(current.data)
+            # result.append(current)
+            current = current.next
+        return result
+
+    def is_empty(self):
+        """Return True if this linked list is empty, or False"""
+        return self.head is None
+
+    def length(self):
+        """Return the length of this linked list by traversing its nodes"""
+        # TODO: count number of items
+        length = 0
+        current = self.head
+        while current is not None:
+            length += 1
+            current = current.next
+        return length
+
+    def append(self, item):
+        """Insert the given item at the tail of this linked list"""
+        # TODO: append given item
+        node = Node(item)
+
+        if self.is_empty():
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            self.tail.next.previous = self.tail
+            self.tail = node
+        return
+
+    def prepend(self, item):
+        """Insert the given item at the head of this linked list"""
+        # TODO: prepend given item
+        node = Node(item)
+
+        if self.is_empty():
+            self.head = node
+            self.tail = node
+        else:
+            node.next = self.head
+            self.head = node
+            self.head.next.previous = self.head
+        return True
+
+    def delete(self, item):
+        """Delete the given item from this linked list, or raise ValueError"""
+        # TODO: find given item and delete if found
+
+        if self.head is None:
+            raise ValueError('Cannot delete from empty Linked List')
+
+        current = self.head
+
+        # If item is first!
+        if current.data == item:
+            if current.next is not None:
+                self.head = current.next
+                self.head.previous = None
+            else:
+                self.head = None
+                self.tail = None
+            return
+
+        while current.next is not None:
+            if current.next.data == item:
+                newNext = current.next.next
+                if newNext is not None:
+                    current.next = newNext
+                    current.next.previous = current
+                else:
+                    current.next = None
+                    self.tail = current
+                return
+            current = current.next
+
+        raise ValueError('Cannot delete this element from the linked list')
+
+    def find(self, quality):
+        """Return an item from this linked list satisfying the given quality"""
+        current = self.head
+        while current is not None:
+            if quality(current.data) == True:
+                return current.data
+            current = current.next
+        return None
+
+    def __contains__(self, item):
+        """Does it contain the item"""
+        current = self.head
+        while current is not None:
+            if current.data == item:
+                return True
+            current = current.next
+        return False
+
+    def __iter__(self):
+        current = self.head
+        while current is not None:
+            yield current
+            current = current.next
+
+
+def test_linked_list():
+    # Test appending
+    print("Testing Appending")
+    ll = LinkedList()
+    print(ll)
+    ll.append('A')
+    print(ll)
+    ll.append('B')
+    print(ll)
+    ll.append('C')
+    print(ll)
+    print('head: ' + str(ll.head))
+    print('tail: ' + str(ll.tail))
+    print(ll.length())
+
+    # Test deleteing
+    print("Testing Deleting")
+    ll.delete('A')
+    print(ll)
+    ll.delete('C')
+    print(ll)
+    ll.delete('B')
+    print(ll)
+    print('head: ' + str(ll.head))
+    print('tail: ' + str(ll.tail))
+    print(ll.length())
+
+    # Test iterable
+    print("Testing Iterable")
+    ll.append('a')
+    ll.append('b')
+    ll.append('c')
+    ll.append('wow')
+    for item in ll:
+        print("prev: ", item.previous)
+        print("item: ", item)
+        print("next: ", item.next)
+        print("\n")
+
+
+if __name__ == '__main__':
+    test_linked_list()
